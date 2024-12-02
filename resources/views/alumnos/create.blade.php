f42856bd370bdc4dffe62746acac5bd0198d61a9
CamiSarmiento <sarmiento_camila@hotmail.com>
2024-12-02 17:49:07 -0300

    termine las validaciones

--- resources/views/alumnos/create.blade.php
+++ resources/views/alumnos/create.blade.php
@@ -6,11 +6,20 @@
     @csrf
     <div class="form-group">
         <label for="nombre">Nombre</label>
-        <input type="text" name="nombre" id="nombre" class="form-control" value="{{ old('nombre') }}">
+        <input type="text" name="nombre" id="nombre" class="form-control @error('nombre') is-invalid @enderror"
+            value="{{ old('nombre') }}">
+        @error('nombre')
+        <small class="text-danger">{{ $message }}</small>
+
+        @enderror
     </div>
     <div class="form-group">
         <label for="apellido">Apellido</label>
-        <input type="text" name="apellido" id="apellido" class="form-control" value="{{ old('apellido') }}">
+        <input type="text" name="apellido" id="apellido" class="form-control @error('apellido') is-invalid @enderror"
+            value="{{ old('apellido') }}">
+        @error('apellido')
+        <small class="text-danger">{{ $message }}</small>
+        @enderror
     </div>
     <div class="form-group">
         <label for="fecha_nacimiento">Fecha de Nacimiento</label>
@@ -30,23 +39,33 @@
     </div>
     <div class="form-group">
         <label for="activo">Activo</label>
-        <select name="activo" id="activo" class="form-control">
+        <select name="activo" id="activo" class="form-control @error('activo') is-invalid @enderror">
             <option value="1">Sí</option>
             <option value="0">No</option>
         </select>
+        @error('activo')
+        <small class="text-danger">{{ $message }}</small>
+        @enderror
     </div>
     <div class="form-group">
         <label for="email">Email</label>
-        <input type="email" name="email" id="email" class="form-control" value="{{ old('email') }}">
+        <input type="email" name="email" id="email" class="form-control @error('email') is-invalid @enderror"
+            value="{{ old('email') }}">
+        @error('email')
+        <small class="text-danger">{{ $message }}</small>
+        @enderror
     </div>
     <div class="form-group">
         <label for="curso_id">Curso</label>
-        <select name="curso_id" id="curso_id" class="form-control">
+        <select name="curso_id" id="curso_id" class="form-control @error('curso_id') is-invalid @enderror">
             <option value="" disabled selected>Seleccione un curso</option>
             @foreach ($cursos as $curso)
             <option value="{{ $curso->id }}">{{ $curso->nombre }}</option>
             @endforeach
         </select>
+        @error('curso_id')
+        <small class="text-danger">{{ $message }}</small>
+        @enderror
     </div>
     <button type="submit" class="btn btn-primary mt-3">Guardar</button>
 </form>
@@ -62,5 +81,4 @@ document.getElementById('fecha_nacimiento').addEventListener('change', function(
     this.value = `${month}/${day}/${year}`; // Formato mes/día/año para ser procesado
 });
 </script>
-
 @endsection
